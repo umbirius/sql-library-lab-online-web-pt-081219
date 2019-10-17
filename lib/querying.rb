62128b878d38dc85f6abdde169f04ebb24f7d7c4
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-17 16:27:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,6 +2,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT title, year 
   FROM books 
   ORDER BY year ASC"
+  WHERE 
 end
 
 def select_name_and_motto_of_char_with_longest_motto
