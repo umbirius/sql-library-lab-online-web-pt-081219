247c3d17082b55481ecbe1991384952310cdd226
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-17 17:53:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,8 +41,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.books_id)
+  "SELECT characters.name, COUNT(character_books.book_id)
   FROM character_books
   JOIN characters ON character_books.character_id = characters.id
-  GROUP BY books.title"
+  GROUP BY characters.name"
 end
