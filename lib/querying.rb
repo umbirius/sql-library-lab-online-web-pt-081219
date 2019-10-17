16eab85964e5a69d4907386b2b92b7d6be68d38e
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-17 16:39:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,8 +24,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors 
-  JOIN series on authors.id = series.author_id 
-  JOIN "
+  JOIN series ON authors.id = series.author_id 
+  JOIN subgenres ON series.subgenre_id = subgenre.id"
 end
 
 def select_series_title_with_most_human_characters
