19598c309c4db6e6e0daa3060b6bed18cb3cf288
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-17 16:33:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
   FROM characters
-  ORDER BY COUNT(species) DESC 
+  ORDER BY COUNT(species) ASC 
   LIMIT 1"
 end
 
