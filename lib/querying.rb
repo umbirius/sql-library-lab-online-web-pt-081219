147434e74608fe6ef19837cc0f934cb4436d68e8
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-17 16:32:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM "
 end
 
 def select_name_and_series_subgenres_of_authors
