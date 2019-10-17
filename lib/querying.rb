2708d9cf45a31b21a44960f352e28f07054dc1b9
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-17 17:53:31 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -44,5 +44,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id)
   FROM character_books
   JOIN characters ON character_books.character_id = characters.id
-  GROUP BY characters.name"
+  GROUP BY characters.name
+  ORDER BY characters.name DESC"
 end
